c1c8453feed6584e02b8838a1c7e8bd46855ae53
WillLang123 <williebear112003@gmail.com>
2025-08-26 08:21:05 -0500

    added simple startup for table template

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request, session, Response
-import dbmgr
+from dbmgr import quickOpen, quickClose
 
 app = Flask(__name__)
 app.secret_key = "DBMSGP"
@@ -9,4 +9,15 @@ app.secret_key = "DBMSGP"
 def index():
     return render_template('index.html')
 
+cursor, conn = quickOpen()
+try:
+    #Create tables here
+    conn.commit()
+    print("Databases created")
+except Exception as e:
+    print("Issue with making database")
+    conn.rollback()
+finally:
+    quickClose(cursor,conn)
+    #hosts website to port 3000 to show website
 app.run(host='0.0.0.0', port=3000, debug=True)
