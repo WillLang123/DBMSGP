60a9a3b0187cc8b4a1d22ac316fe93546a8ca694
WillLang123 <williebear112003@gmail.com>
2025-08-26 08:28:50 -0500

    added template

--- api.py
+++ api.py
@@ -9,13 +9,33 @@ app.secret_key = "DBMSGP"
 def index():
     return render_template('index.html')
 
+# @app.route("/chatroom/<int:chatroomID>/send", methods=["POST"])
+# def handleSendMessage(chatroomID):
+
+# @app.route("/chatroom/<int:chatroomID>/stream")
+# def streamMessages(chatroomID):
+
+# @app.route("/createChatroom", methods=["POST"])
+# def handleCreateChatroom():
+
+# FOR ANY API CALL
+# dataFromAPI = request.get_json()
+# name = dataFromAPI.get('name')
+# cursor, conn = quickCursor()
+# cursor.execute();
+# result = cursor.fetchone()
+# cursor.commit();
+# curso.rollback();
+# output = jsonify({"words": "Blah blag"})
+# return output #so API knows what happened with transaction
+
 cursor, conn = quickOpen()
 try:
     #Create tables here
     conn.commit()
-    print("Databases created")
+    print("Schema created")
 except Exception as e:
-    print("Issue with making database")
+    print("Issue with making Schema")
     conn.rollback()
 finally:
     quickClose(cursor,conn)
